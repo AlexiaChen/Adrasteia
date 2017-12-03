ae39bffbf624333cbbf6cd393243b47e7349990f
MathxH Chen <brainfvck@foxmail.com>
2017-12-03 15:27:42 +0800

    [+]: first react heallo app

---
+++ web-front-end/react-lab/webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+
+module.exports= {
+  entry: {
+    app: path.resolve(APP_PATH, 'app.jsx')
+  },
+  
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+
+  //enable dev source map
+  devtool: 'eval-source-map',
+  //enable dev server
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      js: './app'
+    }
+  },
+  
+  module: {
+   
+  rules: [
+    {
+       test: /\.jsx$/,
+       include: APP_PATH,
+       use: ['eslint-loader', 'babel-loader']
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+  ]
+    
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'My first react app'
+    })
+  ]
+}
